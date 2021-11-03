419c8af67eee6b1df1a6a58079295db30ca98c8b
RDJoseDeSouza <jose441232@gmail.com>
2021-11-03 10:18:48 -0300

    Instalando e começando projeto

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+export const Routes = () =>{
+    return (
+        <Switch>
+            <Route path="/" component={<div>home aqui</div>} exact/>
+            <Route path="/blocks" component={<div>blocks aqui</div>} />
+            <Route path="/cards" component={<div>cards aqui</div>} />
+            <Route path="/forms" component={<div>forms aqui</div>} />
+            <Route path="/pricing" component={<div>pricing aqui</div>} />
+            {/* <Route component={<div>Caminha não encontrado</div>}/> */}
+
+        
+        </Switch>
+        )
+    
+}
