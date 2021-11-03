2c456cc2e2383ffb690372a29e47b00224086fbf
RDJoseDeSouza <jose441232@gmail.com>
2021-11-03 10:39:33 -0300

    Routes funcionado

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,13 @@ import { Switch, Route } from 'react-router-dom'
 export const Routes = () =>{
     return (
         <Switch>
-            <Route path="/" component={<div>home aqui</div>} exact/>
-            <Route path="/blocks" component={<div>blocks aqui</div>} />
-            <Route path="/cards" component={<div>cards aqui</div>} />
-            <Route path="/forms" component={<div>forms aqui</div>} />
-            <Route path="/pricing" component={<div>pricing aqui</div>} />
-            {/* <Route component={<div>Caminha não encontrado</div>}/> */}
+            <Route path="/" component={() =><div>home aqui</div>} exact/>
+            <Route path="/blocks" component={() =><div>blocks aqui</div>} />
+            <Route path="/cards" component={() =><div>cards aqui</div>} />
+            <Route path="/forms" component={() =><div>forms aqui</div>} />
+            <Route path="/pricing" component={() =><div>pricing aqui</div>} />
+            <Route component={() =><div>Caminha não encontrado</div>}/>
 
-        
         </Switch>
         )
     
